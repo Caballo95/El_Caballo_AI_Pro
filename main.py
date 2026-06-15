f10f79709d795e43980026d3e04dd6755cdd3624
Caballo95 <jm6p96pbg5@privaterelay.appleid.com>
2026-06-15 13:21:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -359,14 +359,14 @@ def handle_callback(callback):
         edit_message(chat_id, message_id, "🌙 <b>Selecciona un par OTC:</b>", otc_menu())
 
     elif data == "menu_forex":
-    keyboard = {
-        "inline_keyboard": [
-            [{"text": "EUR/USD", "callback_data": "pair_EUR_USD"}],
-            [{"text": "AUD/USD", "callback_data": "pair_AUD_USD"}],
-            [{"text": "AUD/CAD", "callback_data": "pair_AUD_CAD"}],
-            [{"text": "AUD/CHF", "callback_data": "pair_AUD_CHF"}],
-            [{"text": "AUD/NZD", "callback_data": "pair_AUD_NZD"}],
-            [{"text": "⬅️ Volver", "callback_data": "back_main"}]
+        keyboard = {
+            "inline_keyboard": [
+             [{"text": "EUR/USD", "callback_data": "pair_EUR_USD"}],
+             [{"text": "AUD/USD", "callback_data": "pair_AUD_USD"}],
+             [{"text": "AUD/CAD", "callback_data": "pair_AUD_CAD"}],
+             [{"text": "AUD/CHF", "callback_data": "pair_AUD_CHF"}],
+             [{"text": "AUD/NZD", "callback_data": "pair_AUD_NZD"}],
+             [{"text": "⬅️ Volver", "callback_data": "back_main"}]
         ]
     }
     edit_message(chat_id, message_id, "📈 <b>Selecciona un par Forex mercado real:</b>", keyboard)
